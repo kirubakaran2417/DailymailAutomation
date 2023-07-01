40b1046813f1235578eb531f0c8158defc3ba8ac
k.kirubakaran <k.kirubakaran@stackroute.in>
2023-07-01 22:10:39 +0530

    dailydeal

---
+++ src/main/java/org/dailymail/base/BaseUI.java
@@ -0,0 +1,4 @@
+package org.dailymail.base;
+
+public class BaseUI {
+}
---
+++ src/main/java/org/dailymail/base/DriverSetup.java
@@ -0,0 +1,34 @@
+package org.dailymail.base;
+
+import org.openqa.selenium.UnexpectedAlertBehaviour;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.time.Duration;
+
+public class DriverSetup {
+    private static WebDriver driver;
+
+    /********* Invoke Chrome Driver **********/
+    public static WebDriver getChromeDriver() {
+        ChromeOptions co = new ChromeOptions();
+        co.addArguments("--disable-infobars");
+        co.addArguments("--disable-notifications");
+        co.addArguments("--disable-gpu");
+        co.addArguments("--remote-allow-origins=*");
+        co.setUnhandledPromptBehaviour(UnexpectedAlertBehaviour.DISMISS_AND_NOTIFY);
+        driver = new ChromeDriver(co);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(120));
+        return driver;
+    }
+    /********* Invoke MSEdge Driver **********/
+    public static WebDriver getMSEdgeDriver() {
+        driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(120));
+        return driver;
+    }
+}
